c9859a1ba55a81e2f8f67914835a2751939bc09a
Daniel <danielchukwuma33@gmail.com>
2020-02-19 23:13:05 +0100

    Connection to the Dababase (MongoDB)

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,23 +8,23 @@ router.get("/", (req, res) => {
    res.send("Get all contact");
 });
 
-// @route   POST app/contacts
-// @desc    Get all users contact
-// @access  Private
+// @route      POST app/contacts
+// @desc       Get all users contact
+// @access     Private
 router.post("/", (req, res) => {
    res.send("Get all contact");
 });
 
-// @route   PUT app/contacts/:id
-// @desc    update contact
-// @access  Private
+// @route      PUT app/contacts/:id
+// @desc       Update contact
+// @access     Private
 router.put("/:id", (req, res) => {
    res.send("Upate contact");
 });
 
-// @route   Delete app/contacts/:id
-// @desc    Delete contact
-// @access  Private
+// @route      Delete app/contacts/:id
+// @desc       Delete contact
+// @access     Private
 router.delete("/:id", (req, res) => {
    res.send("Delete contact");
 });
