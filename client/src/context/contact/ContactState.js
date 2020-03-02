0540ce51ce518ff0b085af767c837c07ce51f1e9
Daniel <danielchukwuma33@gmail.com>
2020-03-02 13:02:40 +0100

    Client Side UI

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,71 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+   ADD_CONTACT,
+   DELETE_CONTACT,
+   SET_CURRENT,
+   CLEAR_CURRENT,
+   UPDATE_CONTACT,
+   FILTER_CONTACTS,
+   CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+   const initialState = {
+      contacts: [
+         {
+            id: "1",
+            name: "Daniel Chuks",
+            email: "iamdanielchukwuma@gmail.com",
+            phone: "07037910738",
+            type: "personal"
+         },
+
+         {
+            id: "2",
+            name: "Samuel Sam",
+            email: "samuel@gmail.com",
+            phone: "07031112234",
+            type: "personal"
+         },
+
+         {
+            id: "3",
+            name: "Daniel Chuks",
+            email: "danielchuks@gmail.com",
+            phone: "07083907600",
+            type: "professional"
+         }
+      ]
+   };
+
+   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+   // Add Contact
+
+   // Delete Contact
+
+   // Set Current Contact
+
+   //Clear Current Contact
+
+   // Update Contact
+
+   // Filter Contacts
+
+   // Clear Filter
+
+   return (
+      <ContactContext.Provider
+         value={{
+            contacts: state.contacts
+         }}
+      >
+         {props.children}
+      </ContactContext.Provider>
+   );
+};
+
+export default ContactState;
