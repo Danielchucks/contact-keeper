2d7350d0bc92ea4a171faa9ddd1e11435952c6aa
Daniel <danielchukwuma33@gmail.com>
2020-03-26 22:54:51 +0100

    Login and Register component and Private route for authentication

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Login = () => {
+const Login = props => {
+   const alertContext = useContext(AlertContext);
+   const authContext = useContext(AuthContext);
+
+   const { setAlert } = alertContext;
+   const { login, error, clearError, isAuthenticated } = authContext;
+
+   useEffect(() => {
+      if (isAuthenticated) {
+         props.history.push("/");
+      }
+
+      if (error === "Invalid Credentials") {
+         setAlert(error, "danger");
+         clearError();
+      }
+      // eslint-disable-next-line
+   }, [error, isAuthenticated, props.history]);
+
    const [user, setUser] = useState({
       email: "",
       password: ""
@@ -12,7 +32,14 @@ const Login = () => {
 
    const onSubmit = e => {
       e.preventDefault();
-      console.log("Login Submit");
+      if (email === "" || password === "") {
+         setAlert("Please fill in all fields", "danger");
+      } else {
+         login({
+            email,
+            password
+         });
+      }
    };
 
    return (
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import AuthContext from "./authContext";
 import AuthReducer from "./AuthReducer";
+import setAuthToken from "../../utils/setAuthToken";
 import {
    REGISTER_SUCCESS,
    REGISTER_FAIL,
@@ -51,10 +52,30 @@ const AuthState = props => {
    };
 
    // Login User
-   const login = () => console.log("load User");
+   const login = async formData => {
+      const config = {
+         headers: {
+            "Content-Type": "application/json"
+         }
+      };
+
+      try {
+         const res = await axios.post("./api/auth", formData, config);
+
+         dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+         });
+      } catch (err) {
+         dispatch({
+            type: LOGIN_FAIL,
+            payload: err.response.data.msg
+         });
+      }
+   };
 
    // Logout User
-   const logout = () => console.log("load User");
+   const logout = () => dispatch({ type: LOGOUT });
 
    // Clear Errors
    const clearError = () => dispatch({ type: CLEAR_ERRORS });
